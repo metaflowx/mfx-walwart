1b9fb0050cfeebf2fb35f3212521e04add44e545
cpnsing <cpnsingh8181@gmail.com>
2025-03-05 14:21:55 +0530

    daily bonus and total bonus key added

--- src/ui/notification/roundTab.tsx
+++ src/ui/notification/roundTab.tsx
@@ -65,43 +65,14 @@ export default function roundTab() {
         <Box sx={{ width: '100%', }}>
 
 
-            <Box>
-                <Box sx={{ textTransform: 'capitalize', display: 'flex', justifyContent: 'space-between', alignItems: 'center', flexWrap: 'wrap-reverse', gap: '1rem' }}>
-                    <Tabs
-                        variant="scrollable"
-                        sx={{
-
-                            display: 'inline-block',
-                            padding: '5px 7px 1px 7px',
-                            borderRadius: '6px',
-                            '@media(max-width : 600px)': {
-                                width: '100%',
-                            },
-                            '.MuiTabs-indicator': {
-                                height: 46,
-                                color: '#fff',
-                                background: 'linear-gradient(0deg, #0071CE, #0071CE)',
-                                borderRadius: '30px',
-                                backgroundColor: 'transparent'
-
-                            },
-                            '.Mui-selected': {
-                                color: "#fff !important",
-                                textTransform: 'capitalize',
-                                zIndex: '1',
-                            }
-                        }} value={value} onChange={handleChange} aria-label="basic tabs example">
-                        <Tab sx={{ textTransform: 'capitalize', color: '#0071CE' }} label="Announcement" {...a11yProps(0)} />
-                        <Tab sx={{ textTransform: 'capitalize', color: '#0071CE' }} label="Message"  {...a11yProps(1)} />
-                    </Tabs>
-
-                </Box>
+            <Box pt={2}>
+               
                 <CustomTabPanel value={value} index={0}>
                     <Box sx={{
                         padding: '1rem',
                         borderRadius: '12px',
                         border: '1px solid #DCDCEB',
-                        marginTop:'1rem'
+                       
                     }}>
                         <Typography variant='h6'>ACPINC Team Announcement</Typography>
                         <Typography>2024/10/09 21:30:00</Typography>
