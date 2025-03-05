67eb386fda75b4c5705c2ac8d20b903f4f5db99b
cpnsing <cpnsingh8181@gmail.com>
2025-03-05 14:14:33 +0530

    daily bonus and total bonus key added

--- src/ui/scorecenter/scoreCenter.tsx
+++ src/ui/scorecenter/scoreCenter.tsx
@@ -13,10 +13,10 @@ const {taskList,loading}=taskListData()
   return (
     <>
       <Clicktoback href={"/dashboard/invest"} title={"Scoring Center"} />
-      <Box py={2}>
+      {/* <Box py={2}>
         <DropdownList />
-      </Box>
-      <Grid2 container spacing={2}>
+      </Box> */}
+      <Grid2 container spacing={2} mt={3} >
       {loading ? (
         Array.from(new Array(6)).map((_, index) => (
           <Grid2 key={index} size={{ xs: 12, sm: 6, md: 4 }}>
