924dbc33d1bab302123ef3a5fa4512aa2c2cfe1b
cpnsing <cpnsingh8181@gmail.com>
2025-03-11 18:43:02 +0530

    for code update

--- src/ui/dashboard/Hero.tsx
+++ src/ui/dashboard/Hero.tsx
@@ -9,21 +9,19 @@ const Hero = () => {
     <Box sx={{ width: '100%', borderRadius: '16px', overflow: 'hidden', mt: 2 }}>
       <Swiper
         modules={[Pagination, Autoplay, Navigation]}
-        slidesPerView={1}
         loop={true} 
+        slidesPerView={1}
 
         pagination={{ clickable: true }}
         autoplay={{ delay: 3000, disableOnInteraction: false }}
       >
         {[1, 2, 3].map((index) => (
           <SwiperSlide key={index}>
-            <div className=" flex justify-center items-center bg-black overflow-hidden">
-              <img 
+            <img 
                 src={`/images/home/${index}.jpg`} 
                 alt={`Slide ${index}`} 
-                style={{ width: '100%', height: '400px', objectFit: 'contain' }} 
+                style={{ width: '100%', height: '400px', objectFit: 'contain',borderRadius:"10px" }} 
               />
-            </div>
           </SwiperSlide>
         ))}
       </Swiper>
